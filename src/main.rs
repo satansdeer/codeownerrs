dbdc6a19f25f60b80d8e5956b0a2835880e7bc67
Maksim Ivanov <satansdeer@gmail.com>
2024-02-03 15:22:38 +0100

    Specify CLI api

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
+/// A tool for interacting with GitHub's
+/// [CODEOWNERS](https://help.github.com/articles/about-codeowners/) files.
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[derive(Subcommand)]
+enum Commands {
+    /// Print a list of files in the current repo, followed by its owner
+    Audit {
+        /// Specify a non-standard CODEOWNERS filename
+        #[arg(short, long, default_value_t = String::from("CODEOWNERS"))]
+        file: String,
+
+        /// Max traversal depth
+        #[arg(short, long)]
+        depth: i32,
+
+        /// List the files not covered by the `CODEOWNERS`
+        #[arg(short, long)]
+        unowned: bool,
+    },
+    /// Verify that users/teams own a specific path
+    Verify {
+        /// Path to verify
+        path: Option<String>,
+
+        /// A space-separated list of users/teams 
+        users: Vec<String>,
+
+        /// Specify a non-standard CODEOWNERS filename
+        #[arg(short, long)]
+        file: String,
+    },
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    println!("Hello world!")
 }
